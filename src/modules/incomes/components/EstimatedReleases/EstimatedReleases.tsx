896e796407a18cc90f12a43e3967734d79cca163
Vinicius Rocha <vnc.chelsea@gmail.com>
2026-03-19 01:11:10 -0300

    refactor: remove unnecessary function

--- src/modules/incomes/components/EstimatedReleases/EstimatedReleases.tsx
+++ src/modules/incomes/components/EstimatedReleases/EstimatedReleases.tsx
@@ -79,34 +79,3 @@ export function EstimatedReleases(props: EstimatedReleasesProps) {
     </View>
   );
 }
-function setState(arg0: {
-  (arrayLength: number): ChartData[];
-  (...items: ChartData[]): ChartData[];
-  new (arrayLength: number): ChartData[];
-  new (...items: ChartData[]): ChartData[];
-  isArray(arg: any): arg is any[];
-  readonly prototype: any[];
-  from<T>(arrayLike: ArrayLike<T>): T[];
-  from<T, U>(arrayLike: ArrayLike<T>, mapfn: (v: T, k: number) => U, thisArg?: any): U[];
-  from<T>(iterable: Iterable<T> | ArrayLike<T>): T[];
-  from<T, U>(
-    iterable: Iterable<T> | ArrayLike<T>,
-    mapfn: (v: T, k: number) => U,
-    thisArg?: any,
-  ): U[];
-  of<T>(...items: T[]): T[];
-  fromAsync<T>(
-    iterableOrArrayLike:
-      | AsyncIterable<T>
-      | Iterable<T | PromiseLike<T>>
-      | ArrayLike<T | PromiseLike<T>>,
-  ): Promise<T[]>;
-  fromAsync<T, U>(
-    iterableOrArrayLike: AsyncIterable<T> | Iterable<T> | ArrayLike<T>,
-    mapFn: (value: Awaited<T>, index: number) => U,
-    thisArg?: any,
-  ): Promise<Awaited<U>[]>;
-  readonly [Symbol.species]: ArrayConstructor;
-}): [any, any] {
-  throw new Error('Function not implemented.');
-}
